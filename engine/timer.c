5e8bc32a83cbc09b0929fb86e0b16bd007d9fc32
Exio734 <luis_villa_salas@hotmail.com>
2026-09-24 15:13:15 -0600

    (FEAT) Implementación del temporizador para gestión de tiempo
    - Se añadió la función `timer_init` para inicializar el temporizador.
    - Se implementó `timer_reset` para reiniciar el temporizador.
    - Se creó `timer_get_elapsed_ms` para obtener el tiempo transcurrido en milisegundos.
    - Se agregó `timer_get_time` para formatear y devolver el tiempo transcurrido como una cadena.
    - Se actualizaron los encabezados en `timer.h` para incluir las nuevas funciones.

--- engine/timer.c
+++ engine/timer.c
@@ -1,8 +1,34 @@
 #include <windows.h>
+#include <stdio.h>
+
 #include "timer.h"
+
+static DWORD startTime;
 
 void timer_init() {
     // El sistema crecerá y aqui se manejaran los ticks y una mejor gestion de FPS 
+    startTime = GetTickCount();
+}
+
+void timer_reset() {
+    startTime = GetTickCount();
+}
+
+const char* timer_get_time() {
+    static char timeText[32];
+    unsigned long elapsed = timer_get_elapsed_ms();
+
+    int totalSeconds = elapsed / 1000;
+    int minutes = totalSeconds / 60;
+    int seconds = totalSeconds % 60;
+
+    snprintf(timeText, sizeof(timeText), "TIEMPO: %02d:%02d", minutes, seconds);
+
+    return timeText;
+}
+
+unsigned long timer_get_elapsed_ms() {
+  return GetTickCount() - startTime;
 }
 
 void timer_update() {
--- engine/timer.h
+++ engine/timer.h
@@ -4,4 +4,9 @@
 void timer_init();
 void timer_update();
 
+void timer_reset();
+unsigned long timer_get_elapsed_ms();
+
+const char* timer_get_time();
+
 #endif
